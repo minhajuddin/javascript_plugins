afff09008b092a9480d492103da5617515266383
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-10-08 15:11:09 +0530

    added a build folder with outputs for all the jquery plugins

---
+++ build/jquery.custom.plugins.js
@@ -0,0 +1,81 @@
+﻿(function($) {
+    /* 
+    fillDropDown plugin 
+    fills a drop down box with options, expects a json array of data with a value and text field for every element
+    */
+    $.fn.fillDropDown = function(options) {
+        return this.each(function() {
+            var htmlChunk = '';
+            var i;
+            for (i = 0; i < options.length; i++) {
+                htmlChunk = '<option value="' + options[i].value + '">' + options[i].text + '</option>';
+                $(this).append(htmlChunk);
+            }
+        });
+    };
+
+    /* 
+    enable plugin 
+    enables the selected html input elements
+    */
+    $.fn.enable = function(options) {
+        return this.each(function() {
+            $(this).attr('disabled', null);
+        });
+    };
+
+    /* 
+    disable plugin 
+    disables the selected html input elements
+    */
+    $.fn.disable = function(options) {
+        return this.each(function() {
+            $(this).attr('disabled', 'disabled');
+        });
+    };
+
+    /* 
+    animate form submit button
+    */
+    $.fn.disableWithAnimation = function(options) {
+        var defaults = {
+            cssClass: 'ajax-loader',
+            message: 'Processing..',
+            template: function(message, cssClass) { return '<span class="' + cssClass + '">' + message + '</span>'; }
+        };
+        var opt = $.extend(defaults, options, {});
+        return this.each(function() {
+            if (opt['stop']) {
+                //code to reset the animation
+                $(this).enable();
+                $(this).next().remove();
+            } else {
+                $(this).disable().after(opt['template'](opt['message'], opt['cssClass']));
+            }
+        });
+    };
+
+    /* 
+    allKeys()
+    gives an array of css classes on the current element
+    */
+    $.fn.allKeys = function(key) {
+        var result = [];
+        this.each(function(x) {
+            var val = $(this).metadata()[key];
+            result.push(val);
+        });
+        return result;
+    };
+
+
+    /* 
+    classes()
+    gives an array of css classes on the current element
+    */
+    $.fn.classes = function() {
+        return this.attr('class').split(/\s+/);
+    };
+
+
+} (jQuery));
---
+++ linq/linq.js
@@ -0,0 +1,124 @@
+﻿/*
+linq.js -- a simple LINQ implementation for javascript
+Author: Nate Kohari <nate@enkari.com>
+Copyright (C) 2009 Enkari, Ltd.
+Released under the Apache 2.0 license (http://www.opensource.org/licenses/apache2.0.php)
+*/
+
+Array.prototype.all = function(func) {
+    var result = true;
+    this.iterate(function(item) {
+        if (!func(item)) {
+            result = false;
+            return false;
+        }
+    });
+    return result;
+};
+
+
+Array.prototype.removeAt = function(from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+};
+
+Array.prototype.indexOf = function(element) {
+    for (var i = 0, length = this.length; i < length; i++)
+        if (this[i] === element)
+        return i;
+    return -1;
+};
+
+Array.prototype.remove = function(element) {
+    var index = this.indexOf(element);
+    if (index < 0) return;
+    this.removeAt(index);
+};
+
+
+
+Array.prototype.any = function(func) {
+    var result = false;
+    this.iterate(function(item) {
+        if (func(item)) {
+            result = true;
+            return false;
+        }
+    });
+    return result;
+};
+
+Array.prototype.count = function(func) {
+    if (!func) return this.length;
+    else return this.where(func).length;
+};
+
+Array.prototype.distinct = function(func) {
+    var result = [];
+    var hash = {};
+    this.iterate(function(item) {
+        if (!hash[item]) {
+            result.push(item);
+            hash[item] = true;
+        }
+    });
+    return result;
+};
+
+Array.prototype.orderby = function(func) {
+    var result = [].concat(this);
+    result.sort(function(x, y) {
+        return func.apply(x, [x]) - func.apply(y, [y]);
+    });
+    return result;
+};
+
+Array.prototype.orderbydescending = function(func) {
+    var result = [].concat(this);
+    result.sort(function(x, y) {
+        return func.apply(y, [y]) - func.apply(x, [x]);
+    });
+    return result;
+};
+
+Array.prototype.select = function(func) {
+    var result = [];
+    this.iterate(function(item) {
+        result.push(func(item));
+    });
+    return result;
+};
+
+Array.prototype.selectmany = function(func) {
+    var result = [];
+    this.iterate(function(item) {
+        result.concat(func(item));
+    });
+    return result;
+};
+
+Array.prototype.where = function(func) {
+    var result = [];
+    this.iterate(function(item) {
+        if (func(item)) result.push(item);
+    });
+    return result;
+};
+
+Array.prototype.iterate = function(func) {
+    for (var idx = 0, len = this.length; idx < len; idx++) {
+        if (func.apply(this, [this[idx]]) === false)
+            break;
+    }
+};
+
+Array.prototype.first = function(func) {
+    var result = this.where(func);
+    return result.length > 0 ? result[0] : null;
+};
+
+Array.prototype.include = function(obj) {
+    var result = this.where(function(x) { return x === obj; });
+    return result.length > 0;
+};
